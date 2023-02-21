cbe912fac9164806eb62f38c87c9a2c7bafd534d
SosthenesMS <sosthenesms@yahoo.com.br>
2023-02-21 16:32:27 -0300

    Atualização de pastas

--- ADS - Descomplica/Criação de aplicações e sistemas/AP02.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-	public static void main(String[] args){
-	System.out.println("Tipo de caracter: Debora");
-	System.out.println("Tipo de Inteiro: " + 10);
-	System.out.println("Tipo de Real: " + 10.5);
-	System.out.println("Tipo de Booleno: " + "falso");
-	}
-}
